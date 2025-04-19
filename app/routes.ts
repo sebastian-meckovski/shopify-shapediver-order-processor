c5406b26ac9fe12c4d659618a9f55599f030882a
sebastian-meckovski <sebastian.meckovski@gmail.com>
2025-04-19 21:33:17 +0200

    repurposed the project

--- app/routes.ts
+++
@@ -1,7 +0,0 @@
-import {flatRoutes} from '@remix-run/fs-routes';
-import {layout, type RouteConfig} from '@remix-run/route-config';
-import {hydrogenRoutes} from '@shopify/hydrogen';
-
-export default hydrogenRoutes([
-  layout('./layout.tsx', await flatRoutes()),
-]) satisfies RouteConfig;
